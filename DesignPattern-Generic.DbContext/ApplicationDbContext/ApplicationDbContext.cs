3eea1e82835c8c71572cec52c5ae683dd0a2152f
shraktim <shahadathossainraktim@gmail.com>
2019-02-07 21:12:35 +0600

    application db context added

--- DesignPattern-Generic.DbContext/ApplicationDbContext/ApplicationDbContext.cs
+++ DesignPattern-Generic.DbContext/ApplicationDbContext/ApplicationDbContext.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace DesignPattern_Generic.DbContext.ApplicationDbContext
 {
-    class ApplicationDbContext
+    public class ApplicationDbContext:Microsoft.EntityFrameworkCore.DbContext
     {
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = RAKTIM - PC; Database = DesignPattern; Trusted_Connection = true");
+        }
+       
     }
 }
