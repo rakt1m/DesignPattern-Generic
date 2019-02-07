599578240c81ef2553f9a4bfefdb0f3361f61341
shraktim <shahadathossainraktim@gmail.com>
2019-02-07 20:28:58 +0600

    Class and interfaces added

---
+++ DesignPattern-Generic.DbContext/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_Generic.DbContext.ApplicationDbContext
+{
+    class ApplicationDbContext
+    {
+    }
+}
---
+++ DesignPattern-Generic.Repositories/Repositories/Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_Generic.Repositories.Repositories
+{
+    class Repository
+    {
+    }
+}
